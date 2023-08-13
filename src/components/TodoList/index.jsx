c021e75403a09e7be6a4d5ce8f9809fa62d91343
Vinícius de Oliveira <viniciuss100@hotmail.com>
2023-08-12 23:23:26 -0300

    toDo

---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,22 @@
+import style from "./styles.module.css";
+import icone from "../../assets/trash.svg";
+
+export function TodoList({tarefas}) {
+
+  return (
+    <div className={style.container}>
+      <ul>
+        {tarefas?.map((terefa, index) => {
+          return (
+            <li key={index}>
+              {terefa}
+              <button className={style.btnExcluir}>
+                <img src={icone} />
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
